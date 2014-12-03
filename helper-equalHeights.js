44947a225c8648b4bb322e6be5946c9460850455
Simon Kemmerling <Simpsonight@users.noreply.github.com>
2014-12-03 12:52:50 +0100

    new set height logic
    based on the tutorial from Osvaldas Valutis: http://osvaldas.info/flexbox-based-responsive-equal-height-blocks-with-javascript-fallback

--- helper-equalHeights.js
+++ helper-equalHeights.js
@@ -1,47 +1,47 @@
 /**
- * helper-equalHeights.js - v1.0.0
+ * helper-equalHeights.js - v2.0.0
  * https://github.com/brandung/helper-equalHeights.git
  *
  * Copyright brandung GmbH & Co.KG
  * http://www.brandung.de/
  * MIT License (MIT)
+ * 
+ * @require: jQuery
  *
  * @param {object} block object, e.g. $('ul')
  * @param {string} selector, e.g. 'li'
  */
 
-Brandung.Helpers.equalHeights = function (group, heightSelector) {
+Brandung.Helpers.equalHeights = function (group, item) {
 	if (!group.length) return;
 
-	var currentTallest = 0,
-		currentRowStart = 0,
-		rowDivs = [],
-		topPosition = 0;
+	var s = document.body || document.documentElement, s = s.style;
+	if( s.webkitFlexWrap == '' || s.msFlexWrap == '' || s.flexWrap == '' ) return true;
 
-	group.find(heightSelector).each(function () {
-		var _self = $(this);
+	var $list = group,
+		$items = $list.find(item),
+		setHeights = function ()
+		{
+			$items.css( 'height', 'auto' );
 
-		_self.removeAttr('style');
-		topPosition = _self.position().top;
+			var perRow = Math.floor( $list.width() / $items.width() );
+			if( perRow == null || perRow < 2 ) return true;
 
-		if (currentRowStart != topPosition) {
-			// we just came to a new row.  Set all the heights on the completed row
-			for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
-				rowDivs[currentDiv].height(currentTallest);
+			for( var i = 0, j = $items.length; i < j; i += perRow )
+			{
+				var maxHeight   = 0,
+					$row        = $items.slice( i, i + perRow );
+
+				$row.each( function()
+				{
+					var itemHeight = parseInt( $( this ).outerHeight() );
+					if ( itemHeight > maxHeight ) maxHeight = itemHeight;
+				});
+				$row.css( 'height', maxHeight );
 			}
-			// set the variables for the new row
-			rowDivs.length = 0; // empty the array
-			currentRowStart = topPosition;
-			currentTallest = _self.height();
-			rowDivs.push(_self);
-		} else {
-			// another div on the current row.  Add it to the list and check if it's taller
-			rowDivs.push(_self);
-			currentTallest = (currentTallest < _self.height()) ? (_self.height()) : (currentTallest);
-		}
-		// do the last row
-		for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
-			rowDivs[currentDiv].height(currentTallest);
-		}
-	});
+		};
+
+	setHeights();
+	$(window).on( 'resize', setHeights );
+	$list.find('img').on( 'load', setHeights );
 };
